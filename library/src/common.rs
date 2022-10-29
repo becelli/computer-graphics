8769b39887605ccaef8827a586237e2038588cdf
becelli <gustavobecelli@gmail.com>
2022-10-29 14:30:44 -0300

    removing unused functions and types

--- library/src/common.rs
+++ library/src/common.rs
@@ -1,11 +1,4 @@
 pub type Rgba = [u8; 4];
-
 pub type Point = (u32, u32);
-pub type Color = Rgba;
 pub type Image = Vec<Vec<Rgba>>;
-// pub type DCTCoefficients = Vec<Vec<f32>>;
-pub type Gray = u8;
 
-pub fn rgb2gray(r: u8, g: u8, b: u8) -> Gray {
-    ((r as u16 + g as u16 + b as u16) / 3) as Gray
-}
