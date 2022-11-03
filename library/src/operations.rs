3b8727a9512aa24b2dbc0681b2c354769edeb249
becelli <gustavobecelli@gmail.com>
2022-11-03 16:39:09 -0300

    using bresenham algorithm

--- library/src/operations.rs
+++ library/src/operations.rs
@@ -182,8 +182,10 @@ pub fn draw_circle_parametric(image: Image, p0: Point, p1: Point, color: Rgba) -
 
 pub fn draw_triangle(image: Image, p0: Point, p1: Point, p2: Point, color: Rgba) -> Image {
 
-    let mut new_image = draw_line(image, p0, p1, color);
-    new_image = draw_line(new_image, p1, p2, color);
-    new_image = draw_line(new_image, p2, p0, color);
+    let mut new_image = draw_line_bresenham(image, p0, p1, color);
+    new_image = draw_line_bresenham(new_image, p1, p2, color);
+    new_image = draw_line_bresenham(new_image, p2, p0, color);
     new_image
 }
+
+
